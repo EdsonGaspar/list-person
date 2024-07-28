d36eebf8ad1d9ec69014d2896ede9537e6f2634d
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-07-28 12:49:15 +0100

    first commit, almost done part js

---
+++ js/main.js
@@ -0,0 +1,68 @@
+let participantes =[
+    {
+        name:'Tony',
+        email:'tony@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'edson',
+        email:'edson@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'gaspar',
+        email:'gaspar@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'hadson',
+        email:'hadson@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'luzia',
+        email:'luzia@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'celestina',
+        email:'celestina@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },
+    {
+        name:'ailton',
+        email:'ailton@gmail.com',
+        dataInscricao: new Date(2024, 1, 21, 14, 0),
+        checkIn: new Date(2024, 2, 22, 17, 19)
+    },    
+]
+const criarNovoParticipante = (participante) =>{
+    return ` 
+        <tr>
+            <td>
+                <strong>${participante.name}</strong>
+                <br>
+                <small>${participante.email}</small>
+            </td>
+            <td>${participante.dataInscricao}</td>
+            <td>${participante.checkIn}</td>
+        </tr>`
+}
+const actualizarLista = (participantes) =>{
+    let outpot= "";
+    for(let participante of participantes){
+        outpot += criarNovoParticipante(participante);
+    }
+    // alert('Estou dentro da função');
+    const listaParticipante = document.getElementById('listaPart');
+
+    listaParticipante.innerHTML = outpot 
+}
+
+actualizarLista(participantes);
